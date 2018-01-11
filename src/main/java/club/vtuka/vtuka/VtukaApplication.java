85696473d56a439ece4f22008f2d872e3478a260
Vvvvvvvvvvvvvvvvicky <vicky.h.m.lee@outlook.com>
2018-01-11 23:14:14 +0800

    init

---
+++ src/main/java/club/vtuka/vtuka/VtukaApplication.java
@@ -0,0 +1,12 @@
+package club.vtuka.vtuka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtukaApplication.class, args);
+	}
+}
